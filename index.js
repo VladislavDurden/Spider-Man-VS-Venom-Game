0e27e93a4050835e1c258a1a8fca52a1da00aac5
VladislavDurden <petrovskiy.bsuir@gmail.com>
2018-10-03 16:41:56 +0300

    another one

--- index.js
+++ index.js
@@ -31,40 +31,77 @@ var rotate = function (deg) {
     document.getElementById('hero').style.transform = 'rotate(' + deg + 'deg)';
 };
 
+var find =  function(array, value) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] == value) return false;
+  }
+  return true;
+};
+
 // Управление с клавиатуры
 document.addEventListener('keydown', event => {
+    
+    const LEFT_BORDER =[0,7,14,21,28,35,42];
+    const TOP_BORDER =[0,1,2,3,4,5,6];
+    const RIGHT_BORDER=[6,13,20,27,34,41,48];
+    const BOTTOM_BORDER=[42,43,44,45,46,47,48];
+    
+    
 
     // если нажата клавиша влево
     if (event.keyCode === 37) {
-        del(start);
-        start--;
-        push(start);
-        rotate(-90);
-
+        
+        if(find(LEFT_BORDER, start)){
+            del(start);
+            start--;
+            push(start);
+            rotate(-90);
+        } else{
+            rotate(-90);
+        }
+        
     }
     // если нажата клавиша вправо
     else if (event.keyCode === 39) {
-        del(start);
-        start++;
-        push(start);
-        rotate(90);
+        
+        if(find(RIGHT_BORDER, start)){
+            del(start);
+            start++;
+            push(start);
+            rotate(90);
+        } else{
+            rotate(90);
+        }
     }
     // если нажата клавиша вверх
     else if (event.keyCode === 38) {
-        del(start);
-        for (i = 0; i < 7; i++) {
+        if(find(TOP_BORDER, start)){
+            del(start);
+            for (i = 0; i < 7; i++) {
             start--;
         }
-        push(start);
+            push(start);
+            
+        } else{
+            rotate(0);
+        }
     }
+    
+    
+    
     // если нажата клавиша вниз
     else if (event.keyCode === 40) {
-        del(start);
-        for (i = 0; i < 7; i++) {
+        if(find(BOTTOM_BORDER, start)){
+            del(start);
+            for (i = 0; i < 7; i++) {
             start++;
         }
-        push(start);
-        rotate(180);
+            push(start);
+            rotate(180);
+            
+        } else{
+            rotate(180);
+        }
     }
 
 });
